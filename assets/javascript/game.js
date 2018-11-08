e16cefd44cb817ce09e3eae3edd11cd2588f94f8
matttali09 <matttaliancich09@gmail.com>
2018-11-08 16:08:44 -0500

    added some moree of my logic from my psychic game and added a current word placeholder

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,14 +5,20 @@ var wins = 0;
 var losses = 0;
 var turns = 9;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var wordBank = ["bookkeeper", "chophouse", "handkerchief", "hangman", "buffoonery", "spastic", "anxiety", "endowment"]
+var wordBank = ["bookkeeper", "chophouse", "handkerchief", "hangman", "buffoonery", "spastic", "anxiety", "endowment"];
 
 
 // Initialize a variable for the computers random pick of a letter
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(computerChoice)
+var computerChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
+console.log(computerChoice);
+// set a restart function
+function startPoint() {
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice);
+}
 
 // Initialize variables that hold references to the places in the HTML where we want to display things.
+var currentWordText = document.getElementById("current-word");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
@@ -21,21 +27,28 @@ var guessesSoFarText = document.getElementById("guesses-so-far-text");
 // Create the function for the events and what happens on the events
 document.onkeyup = function (event) {
 
-    // Determine which key is pressed and check it to the computer's choice
-     var userGuess = event.key;
-     if (userGuess === computerChoice) {
+    // Determine which key is pressed and check it to the computer's choice 
+    // add toLowerCase to clean user input
+    var userGuess = event.key.toLowerCase();
+    userGuess
+    if (userGuess === computerChoice) {
         wins++;
         winsText.textContent = "Wins: " + wins;
+        turns = 9;
+        guessesSoFarText.textContent = "";
+        startPoint();
     }
     else {
         turns--;
         guessesLeftText.textContent = "Guesses Left: " + turns;
+        guessesSoFarText.textContent += userGuess + ", ";
     }
     if (turns === 0) {
         losses++;
         lossesText.textContent = "Losses: " + losses;
+        turns = 9;
+        guessesSoFarText.textContent = "";
+        startPoint();
     }
-
-    guessesSoFarText = "k"
 
 }
